b8ee42d87ede56978815a2bb2d17580f262dfee8
patosButNotDuck <colab4hashim@gmail.com>
2025-12-23 00:53:09 +0000

    amended issue due to hardcoded nature of csv write

--- C_gain__W_Travel.m
+++ C_gain__W_Travel.m
@@ -109,75 +109,39 @@ for row_idx = 1:size(values_data, 1)
         valid_combinations(end+1, :) = [theta, best_alpha];
     end
     
-    % START of requested debugging block for row 6
-    if row_idx == 6
-        disp('--- Debugging Info for Row 6 ---');
-
-        % --- Values at theta 0 ---
-        theta_0 = 0;
-        % Find the best_alpha for theta=0 from the results
-        alpha_at_0 = valid_combinations(valid_combinations(:,1) == theta_0, 2);
-        
-        % Recalculate values for theta=0 and its best_alpha
-        Lx_0 = n + (l.*cosd(theta_0)) - ((y1-hr).*sind(theta_0));
-        Ly_0 = hr + (l.*sind(theta_0)) + ((y1-hr).*cosd(theta_0));
-        Ux_0 = m + (u.*cosd(alpha_at_0)) - ((y2-sumh).*sind(alpha_at_0));
-        Uy_0 = sumh + (u.*sind(alpha_at_0)) + ((y2-sumh).*cosd(alpha_at_0));
-        distLim_0 = sqrt((Lx_0 - Ux_0).^2 + (Ly_0 - Uy_0).^2);
-        
-        dx_0 = n + ((l - od).*cosd(theta_0)) - ((y1-hr).*sind(theta_0));
-        dy_0 = hr + ((l - od).*sind(theta_0)) + ((y1-hr).*cosd(theta_0));
-        dxf_0 = (n + l - od) + (d.*(sind(thetad)));
-        dyf_0 = y1 + (d.*(cosd(thetad)));
-        decomp_0 = sqrt(((dxf_0 - dx_0).^2) + ((dyf_0 - dy_0).^2));
-        
-        fprintf('distLim at theta 0: %f\n', distLim_0);
-        fprintf('decomp at theta 0: %f\n', decomp_0);
-        
-        % --- distLim at theta 10 ---
-        theta_10 = 10;
-        % Find the best_alpha for theta=10 from the results
-        alpha_at_10 = valid_combinations(valid_combinations(:,1) == theta_10, 2);
-        
-        % Recalculate distLim for theta=10 and its best_alpha
-        Lx_10 = n + (l.*cosd(theta_10)) - ((y1-hr).*sind(theta_10));
-        Ly_10 = hr + (l.*sind(theta_10)) + ((y1-hr).*cosd(theta_10));
-        Ux_10 = m + (u.*cosd(alpha_at_10)) - ((y2-sumh).*sind(alpha_at_10));
-        Uy_10 = sumh + (u.*sind(alpha_at_10)) + ((y2-sumh).*cosd(alpha_at_10));
-        distLim_10 = sqrt((Lx_10 - Ux_10).^2 + (Ly_10 - Uy_10).^2);
-        fprintf('distLim at theta 10: %f\n', distLim_10);
-        
-        % --- decomp at theta 15 and best_alpha for that row ---
-        theta_15 = 15;
-        % Find the best_alpha for theta=15 from the results
-        alpha_at_15 = valid_combinations(valid_combinations(:,1) == theta_15, 2);
-        
-        % Recalculate decomp for theta=15
-        dx_15 = n + ((l - od).*cosd(theta_15)) - ((y1-hr).*sind(theta_15));
-        dy_15 = hr + ((l - od).*sind(theta_15)) + ((y1-hr).*cosd(theta_15));
-        dxf_15 = (n + l - od) + (d.*(sind(thetad)));
-        dyf_15 = y1 + (d.*(cosd(thetad)));
-        decomp_15 = sqrt(((dxf_15 - dx_15).^2) + ((dyf_15 - dy_15).^2));
-        fprintf('decomp at theta 15 is: %f, with best_alpha: %f\n', decomp_15, alpha_at_15);
-        
-        disp('--- End of Debugging Info ---');
-    end
-    % END of requested debugging block
+    % Determine the expected number of columns for thetas and alphas from 'sweep'
+    num_expected = sweep + 1;
     
-    if size(valid_combinations, 1) == 16
-        thetas = valid_combinations(:, 1)'; % Transpose to a 1x16 row vector
-        alphas = valid_combinations(:, 2)'; % Transpose to a 1x16 row vector
-        constants_row = [m, n, l_length, u_length, hr, sumh, y1, y2];
-        output_row = [constants_row, thetas, alphas];
-        
-        % Append the data row to the CSV using an Octave-compatible function
-        dlmwrite('c_gain_rear.csv', output_row, '-append', 'delimiter', ',');
-    else
-        fprintf('Warning: Did not find 16 valid combinations for input row %d. Skipping output for this row.\n', row_idx);
-    end
+    % Initialize theta and alpha arrays with zeros, which will be used for padding
+    thetas = zeros(1, num_expected);
+    alphas = zeros(1, num_expected);
+    
+    num_combinations = size(valid_combinations, 1);
 
-end
+    if num_combinations > 0
+        % Get the actual combinations found
+        actual_thetas = valid_combinations(:, 1)';
+        actual_alphas = valid_combinations(:, 2)';
+        
+        % Determine how many values to copy over (handles both padding and truncation)
+        len_to_copy = min(num_combinations, num_expected);
+        
+        thetas(1:len_to_copy) = actual_thetas(1:len_to_copy);
+        alphas(1:len_to_copy) = actual_alphas(1:len_to_copy);
+
+        if num_combinations > num_expected
+            fprintf('Warning: Found %d valid combinations, but space for only %d. Truncating for input row %d.\n', num_combinations, num_expected, row_idx);
+        end
+    end
+    
+    % Construct the full row for the CSV
+    constants_row = [m, n, l_length, u_length, hr, sumh, y1, y2];
+    output_row = [constants_row, thetas, alphas];
+    
+    % Append the data row to the CSV, ensuring a consistent number of columns
+    dlmwrite('c_gain_rear.csv', output_row, '-append', 'delimiter', ',');
 
 endtime = toc();
 
 disp(['Done, Time taken: ' num2str(endtime) ' seconds.'])
+
